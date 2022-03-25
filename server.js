8c4d2cb9d7a65dd18e1df4defbf594f294c24fa8
Peter Na <ruikangna0605@hotmail.com>
2022-03-25 06:49:04 -0700

    server display everything

--- server.js
+++ server.js
@@ -1,12 +1,43 @@
 const http = require('http')  // http 
 const fs = require('fs')  // file handler
+const path = require('path');  // PATH finder
 const port = 3000;
 
 // create server, the function that is passed to createServer will be called whenever we request from the server
 const server = http.createServer((req, res) => {
+  let filePath = path.join(__dirname, "public", req.url === "/" ? "index.html" : req.url)
+  let extName = path.extname(filePath)
+  let contentType = 'text/html'
+
+  switch (extName) {
+    case '.css':
+        contentType = 'text/css';
+        break;
+    case '.js':
+        contentType = 'text/javascript';
+        break;
+    case '.json':
+        contentType = 'application/json';
+        break;
+    case '.png':
+        contentType = 'image/png';
+        break;
+    case '.jpg':
+        contentType = 'image/jpg';
+        break;
+  }
+
+  console.log(`File path: ${filePath}`);
+  console.log(`Content-Type: ${contentType}`)
+
   // Status code: 200
   // JSON: specify type of content to send for parsing
-  res.writeHead(200, { 'Content-Type': 'text/html' })  
+  res.writeHead(200, { 'Content-Type': contentType })  
+
+  const readStream = fs.createReadStream(filePath);
+  readStream.pipe(res);
+})
+/*
   fs.readFile('index.html', (error, data) => {
     // error: error if any arises 
     // data: all the data of the specified file
@@ -19,9 +50,9 @@ const server = http.createServer((req, res) => {
 
     res.end()  // ending server
   })
-})
+})*/
 
-server.listen(port, function(error) {
+server.listen(port, (error) => {
   if (error) {
     console.log('ERROR: failed to get information', error)
   } else {
