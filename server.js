604475b8bafd317624f421c6b62ff899bd4eec99
Peter Na <ruikangna0605@hotmail.com>
2022-03-28 13:22:50 -0700

    server working remotely

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const http = require('http')  // http 
-const fs = require('fs')  // file handler
-const vm = require('vm')  // virtual machine 
-const path = require('path') // PATH finder
-const port = 3000;
+
+const http = require('http');  // http 
+const fs = require('fs'); // file handler
+const path = require('path');  // PATH finder
+const hostname = 'cpen291-17.ece.ubc.ca';
+const port = 80;
 
 // create server, the function that is passed to createServer will be called whenever we request from the server
 const server = http.createServer((req, res) => {
@@ -38,22 +39,8 @@ const server = http.createServer((req, res) => {
   const readStream = fs.createReadStream(filePath);
   readStream.pipe(res);
 })
-/*
-  fs.readFile('index.html', (error, data) => {
-    // error: error if any arises 
-    // data: all the data of the specified file
-    if (error) {
-      res.writeHead(404)
-      res.write('ERROR: File not found')
-    } else {
-      res.write(data)  // write back to server requested data
-    }
 
-    res.end()  // ending server
-  })
-})*/
-
-server.listen(port, (error) => {
+server.listen(port, hostname, (error) => {
   if (error) {
     console.log('ERROR: failed to get information', error)
   } else {
@@ -61,53 +48,3 @@ server.listen(port, (error) => {
   }
 })
 
-
-
-
-
-
-
-
-/*
-// var http = require('http');
-// var fs = require('fs');
-
-
-// //create a server object:
-// http.createServer(function handler (req, res) {
-//     fs.readFile('./index.html', function(err, data) { //read file index.html in public folder
-//         if (err) {
-//           res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
-//           return res.end("404 Not Found");
-//         }
-//         res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
-//         res.write(data); //write data from index.html
-//         return res.end();
-//     });
-// }).listen(8080); //the server object listens on port 8080
-
-function move_forward(){
-
-  document.getElementById("forward").value ="forward"
-}
-
-function move_backward(){
-  document.getElementById("backward").value ="backward"
-}
-
-function move_right(){
-  document.getElementById("right").value ="right"
-}
-
-function move_left(){
-  document.getElementById("left").value ="left"
-}
-
-// document.getElementById("forward").addEventListener("keydown", (w) =>{
-//   document.getElementById("forward").value ="forward"
-// });
-
-// // document.getElementById("forward").addEventListener("keyup", (w) =>{
-// //   document.getElementById("forward").value ="w"
-// // });
-*/
