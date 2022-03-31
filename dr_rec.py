2163259931145e122dd0f5ee13beaf89b7128d08
sonya <sonyazhao02@gmail.com>
2022-03-31 02:43:17 +0100

    other shapes ok

---
+++ dr_rec.py
@@ -0,0 +1,76 @@
+import sys
+import time
+from tracemalloc import stop
+from turtle import pd
+from urllib.parse import DefragResultBytes
+import board
+from adafruit_motorkit import MotorKit
+import RPi.GPIO as GPIO
+
+servoPin = 20
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPin, GPIO.OUT)
+kit = MotorKit(i2c=board.I2C())
+
+p = GPIO.PWM(servoPin, 50) # GPIO 17 for PWM with 50Hz
+p.start(2.5) # Initialization
+
+def stop():
+    kit.motor1.throttle = 0
+    kit.motor2.throttle = 0
+    time.sleep(1)
+    
+def pdown():
+    p.ChangeDutyCycle(5.5)
+    kit.motor1.throttle = 0
+    kit.motor2.throttle = 0
+    time.sleep(1)
+    
+
+def pup():
+    p.ChangeDutyCycle(10)
+    kit.motor1.throttle = 0
+    kit.motor2.throttle = 0
+    time.sleep(1)
+    
+def forward(interval):
+    kit.motor1.throttle = 0.50 # m1 is right side
+    kit.motor2.throttle = 0.50
+    time.sleep(interval)
+    kit.motor1.throttle = 0
+    kit.motor2.throttle = 0
+    
+def turndegree(degree_interval, back_interval):  # turn right
+    # the goal is to turn 90, without moving the pen from it's originial position on paper
+    # assume the pen is down before starting this function
+    pup()
+    forward(0.05)
+    kit.motor2.throttle = 0.5
+    time.sleep(degree_interval) #0.845 for square?
+    stop()
+    kit.motor1.throttle = -0.50  # backup
+    kit.motor2.throttle = -0.50
+    time.sleep(back_interval) #0.75
+    kit.motor1.throttle = 0
+    kit.motor2.throttle = 0
+    time.sleep(0.5)
+
+# car-> 0__1
+#       |  |
+#       ````
+pdown()
+forward(0.3)  # 0 to 1
+turndegree(0.59,0.6)
+pdown()
+forward(0.6)  # 2
+turndegree(0.59,0.6)
+pdown()
+forward(0.3)  # 3 
+turndegree(0.59,0.6)
+pdown()
+forward(0.6)  # 4
+turndegree(0.59,0.6) # reposition: facing to right
+pup()
+
+p.stop()
+GPIO.cleanup() #free resources on gpio pins 
